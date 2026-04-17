9272ec4a2062d03ab9985e714933df9f5dfc5eee
“Collinda <stgermco@sheridancollege.ca>
2026-04-17 17:10:09 -0400

    foundation for Library class

---
+++ LibraryApp_Interactive/LibraryAppInteractive/LibraryApp/BusinessLogic/Library.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryAppInteractive.BusinessLogic
+{
+    public class Library
+    {
+        #region Fields
+        private List<Book> _bookList;
+        private int _libIDGeneratorSeed;
+        private const int DEFAULT_LIBID_START = 100;
+        #endregion
+
+        #region Constructors
+        public Library()
+        {
+            _bookList = new List<Book>();
+            _libIDGeneratorSeed = DEFAULT_LIBID_START;
+        }
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+        private void CreateDefaultBook()
+        {
+
+        }
+
+        private int DetermineLibID()
+        {
+
+        }
+
+        public Book RegisterBook(string bookName, string bookISBN, string[] authors, BookType bookType, int nCopies)
+        {
+
+        }
+
+        public Book FIndBookByName(string bookName)
+        {
+
+        }
+
+        public Book FindBookByISBN(string bookISBN)
+        {
+
+        }
+        #endregion
+    }
+}
