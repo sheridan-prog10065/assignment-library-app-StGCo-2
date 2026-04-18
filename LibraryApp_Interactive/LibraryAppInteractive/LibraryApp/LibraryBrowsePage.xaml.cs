da7015d082c0d52832e45451205f9dd0a559127d
“Collinda <stgermco@sheridancollege.ca>
2026-04-18 16:22:08 -0400

    worked on search button

--- LibraryApp_Interactive/LibraryAppInteractive/LibraryApp/LibraryBrowsePage.xaml.cs
+++ LibraryApp_Interactive/LibraryAppInteractive/LibraryApp/LibraryBrowsePage.xaml.cs
@@ -6,18 +6,50 @@ public partial class LibraryBrowsePage : ContentPage
 {
     private Library _library;
     private Book _book;
+    private List<Book> _bookList;
     public LibraryBrowsePage()
     {   
         InitializeComponent();
-        _library = new Library();
+        _lstBooks.ItemsSource = _bookList;
     }
 
-    //public OnSearchBook()
+    private async Task OnSearchBook(object sender, EventArgs e)
+    {
+        if (sender == _btnSearch)
+        {
+            string bookName = _entName.Text;
+            string bookISBN = _entISBN.Text;
+            try
+            {
+                if (_entName != null)
+                {
+                    _library.FIndBookByName(bookName);
+                }
 
+                if (_entISBN != null)
+                {
+                    _library.FindBookByISBN(bookISBN);
+                }
+                return;
+            }
+            catch (ArgumentNullException ex)
+            {
+                await DisplayAlertAsync("Browse Page", ex.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlertAsync("Browser Page", ex.Message, "OK");
+            }
+        }
+    }
 
-    // public OnBorrowBook()
+    private void OnBorrowBook(object sender, EventArgs e)
+    {
+        _book.BorrowBook();
+    }
 
+    private void OnReturnBook(object sender, EventArgs e)
+    {
 
-    // public OnReturnBook()
-
+    }
 }
